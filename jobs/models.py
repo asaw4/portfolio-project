e7c1d6b7dfdd1d494297d0b28177e1df607a7a48
Astha Awasthi <asthaaw04@gmail.com>
2019-10-29 22:47:33 +0530

    updated model job

--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 # Create your models here.
 class Job(models.Model):
-	image=models.ImageField(upload_to='MEDIA_ROUTE')
+	image=models.ImageField(upload_to='MEDIA_ROOT')
 	summary=models.CharField(max_length=200)
 
