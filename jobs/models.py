f8e0e968611d97b24e6d3e894396d0c1542d9a5e
Astha Awasthi <asthaaw04@gmail.com>
2019-10-29 21:14:04 +0530

    added models

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Job(models.Model):
-	image=models.ImageField(upload_to='')
+	image=models.ImageField(upload_to='MEDIA_ROUTE')
 	summary=models.CharField(max_length=200)
 
