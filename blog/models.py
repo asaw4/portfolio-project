7b9d2c76f4cf09a7556d128aa4979304a37c3a55
Astha Awasthi <asthaaw04@gmail.com>
2019-10-30 16:18:57 +0530

    added model Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Blog(models.Model):
+	title=models.CharField(max_length=250)
+	pub_date=models.DateTimeField()
+	text=models.TextField()
+	image=models.ImageField(upload_to='MEDIA_ROOT')
+
